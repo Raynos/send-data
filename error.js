8e6f12fa36597da8fa87d9f4e3a9f15b61ab55c8
Raynos <raynos2@gmail.com>
2013-05-08 17:02:08 -0700

    allow setting statusCode

--- error.js
+++ error.js
@@ -2,11 +2,11 @@ var sendJson = require("./json")
 
 module.exports = sendError
 
-function sendError(req, res, error) {
+function sendError(req, res, error, statusCode) {
     var message = typeof error === "string" ? error : error.message
 
     sendJson(req, res, {
-        statusCode: 500,
+        statusCode: statusCode || 500,
         body: { message: message }
     })
 }
