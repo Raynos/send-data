90d3309ea92543620c8fe49d4d7d67ef1d5966a9
Raynos <raynos2@gmail.com>
2013-08-01 13:23:26 -0700

    remove comment

--- test/integration.js
+++ test/integration.js
@@ -61,7 +61,6 @@ function startTest(request, done) {
 
     test("json", function (t) {
         request("/json", function (err, res, body) {
-            console.log("wut", res.statusCode)
             var data = JSON.parse(body)
 
             t.equal(data.foo, "bar")
