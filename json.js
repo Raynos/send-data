c44844f3c2edab21abd81ffdd168c0124397ef73
Raynos <raynos2@gmail.com>
2014-08-19 15:36:11 -0700

    better json printer

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
-var stringify = require("json-stringify-safe")
+// var stringify = require("json-stringify-safe")
 
 var send = require("./index")
 var isSendObject = require("./is-send-object")
@@ -15,10 +15,29 @@ function sendJson(req, res, opts, callback) {
         opts.space = "    "
     }
 
+    var tuple = safeStringify(opts.body,
+        opts.replacer || null, opts.space || "");
+
+    if (tuple[0]) {
+        return callback(tuple[0]);
+    }
+
     opts.headers = opts.headers || {}
-    opts.body = stringify(opts.body,
-        opts.replacer || null, opts.space || "")
+    opts.body = tuple[1];
     opts.headers["Content-Type"] = "application/json"
 
     send(req, res, opts, callback)
+}
+
+function safeStringify(obj, replace, space) {
+    var json;
+    var error = null;
+
+    try {
+        json = JSON.stringify(obj, replace, space);
+    } catch (e) {
+        error = e;
+    }
+
+    return [error, json];
 }
